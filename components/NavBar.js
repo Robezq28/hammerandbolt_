e6b91662df4f3e6c57011926816920e3ae65b6f7
Roberto Ezquerro <roberto.ezquerro@gmail.com>
2023-05-19 16:21:23 -0500

    Fixes

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,35 +1,51 @@
 export default function NavBar() {
   return (
-    <div className='flex flex-col items-start justify-start fixed top-0 w-full z-50'>
-      <h1 className='text-neutral-100 text-4xl sm:text-6xl font-medium pt-4'>
-        hammerandbolt<span className='text-red-500 animate-pulse'>_</span>
-      </h1>
-      <nav className='flex flex-col sm:flex-col py-4'>
+    <div>
+      <div className='flex flex-col items-start justify-start fixed top-0 w-full z-50'>
         <a
           href='/'
-          className='text-neutral-100 text-2xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
+          className='text-neutral-100 text-4xl sm:text-6xl font-medium py-4'
         >
-          work<span className='text-red-500 group-hover:animate-pulse'>_</span>
+          hammerandbolt<span className='text-red-500 animate-pulse'>_</span>
         </a>
-        <a
-          href='/'
-          className='text-neutral-100 text-2xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
-        >
-          work<span className='text-red-500 group-hover:animate-pulse'>_</span>
-        </a>
-        <a
-          href='/'
-          className='text-neutral-100 text-2xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
-        >
-          work<span className='text-red-500 group-hover:animate-pulse'>_</span>
-        </a>
-        <a
-          href='/'
-          className='text-neutral-100 text-2xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
-        >
-          work<span className='text-red-500 group-hover:animate-pulse'>_</span>
-        </a>
-      </nav>
+        <nav className='flex py-4 flex-col sm:flex-col'>
+          <a
+            href='/'
+            className='text-neutral-100 text-3xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
+          >
+            about
+            <span className='text-red-500 group-hover:animate-pulse'>_</span>
+          </a>
+          <a
+            href='/'
+            className='text-neutral-100 text-3xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
+          >
+            work
+            <span className='text-red-500 group-hover:animate-pulse'>_</span>
+          </a>
+          <a
+            href='/'
+            className='text-neutral-100 text-3xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
+          >
+            expertise
+            <span className='text-red-500 group-hover:animate-pulse'>_</span>
+          </a>
+          <a
+            href='/'
+            className='text-neutral-100 text-3xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
+          >
+            contact
+            <span className='text-red-500 group-hover:animate-pulse'>_</span>
+          </a>
+          <a
+            href='/'
+            className='text-neutral-100 text-3xl font-medium group block mb-2 sm:mb-0 sm:mr-4'
+          >
+            carrers
+            <span className='text-red-500 group-hover:animate-pulse'>_</span>
+          </a>
+        </nav>
+      </div>
     </div>
   );
 }
