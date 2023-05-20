b8f5e13c44bd8032464072a8e36ee2fe5f0187ce
Roberto Ezquerro <roberto.ezquerro@gmail.com>
2023-05-20 11:10:02 -0500

    Fixes

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div className='flex flex-col gap-y-4 justify-center items-center m-auto h-screenbg-black'>
-      <span className='text-black dark:text-neutral-50 text-6xl font-bold font-nunito flex items-center justify-center animate-pulse '>
+    <div className='flex flex-col gap-y-4 justify-center items-center m-auto h-screen bg-black'>
+      <span className='text-black dark:text-neutral-50 text-2xl sm:text-6xl font-bold font-nunito flex items-center justify-center animate-pulse '>
         hammerandbolt
         <span className='text-6xl text-red-500 font-serif flex items-center'>
           _
