4256cabdff9b7d36555258053ef6ff5275c800d8
Roberto Ezquerro <roberto.ezquerro@gmail.com>
2023-05-20 01:07:50 -0500

    Navbar

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
 import NavBar from "@/components/NavBar";
-import SphereGeometry from "@/components/SphereGeometry";
-import TorusKnotGeometry from "@/components/TorusKnotGeometry";
 
 export default function Home() {
   return (
